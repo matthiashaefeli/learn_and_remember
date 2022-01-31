cba2366e2d88b5e6a39dca5c8f0b00ab3ed93406
Matthias Haefeli <haefeli.matthias@gmail.com>
2022-01-31 17:45:43 -0600

    sign in user graphql test (#22)
    Co-authored-by: matthiashaefeli <haefeli.matthias2gmail.com>

---
+++ spec/graphql/mutations/users/sign_in_user_spec.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+module Mutations
+  module Users
+    RSpec.describe SignInUser, type: :request do
+      let(:user) { create(:user) }
+      describe '.resolve' do
+        it 'sign in a user' do
+          post '/graphql', params: { query: query(name: user.name, email: user.email, password: user.password) }
+          json = JSON.parse(response.body)
+          user = json['data']['signInUser']['user']
+          authenticate = json['data']['signInUser']['authenticate']
+          expect(user).to include(
+            'name' => be_present,
+            'email' => be_present,
+            'verified' => false
+          )
+          expect(authenticate).to include(
+            'token' => be_present
+          )
+        end
+      end
+
+      def query(name:, email:, password:)
+        <<~GQL
+          mutation {
+            signInUser(input: { params: {
+              name: "#{name}",
+              email: "#{email}",
+              password: "#{password}" }}) {
+              authenticate {
+                token
+              }
+              user {
+                name
+                email
+                verified
+              }
+            }
+          }
+        GQL
+      end
+    end
+  end
+end
