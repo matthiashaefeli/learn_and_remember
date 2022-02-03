bdc9e064ae4c56b1f5c4fed567cd7d63f52d7ac7
Matthias Haefeli <haefeli.matthias@gmail.com>
2022-02-02 19:47:23 -0600

    add_user update test (#28)
    Co-authored-by: matthiashaefeli <haefeli.matthias2gmail.com>

--- spec/graphql/mutations/users/add_user_spec.rb
+++ spec/graphql/mutations/users/add_user_spec.rb
@@ -6,10 +6,16 @@ module Mutations
   module Users
     RSpec.describe AddUser, type: :request do
       describe '.resolve' do
-        it 'creates a user' do
+        it 'create a user' do
           expect do
             post '/graphql', params: { query: query(name: 'test', email: 't@t.com', password: 'pass') }
           end.to change { User.count }.by(1)
+        end
+
+        it 'can not create a user' do
+          post '/graphql', params: { query: query(name: '', email: 't@t.com', password: 'pass') }
+          error_response = JSON.parse(response.body)['errors'][0]['message']
+          expect(error_response).to eq "Invalid attributes for User: Name can't be blank"
         end
       end
 
