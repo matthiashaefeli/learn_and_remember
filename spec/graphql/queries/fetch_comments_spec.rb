b73ad9dc5268d3dab2dc7260033f6e3fb9f4abb4
Matthias Haefeli <haefeli.matthias@gmail.com>
2022-02-04 15:24:37 -0600

    fetch comments test (#32)
    Co-authored-by: matthiashaefeli <haefeli.matthias2gmail.com>

---
+++ spec/graphql/queries/fetch_comments_spec.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require './spec/support/sign_in_user_helper'
+
+RSpec.configure do |c|
+  c.include SignInUserHelper
+end
+
+module Queries
+  RSpec.describe FetchComments, type: :request do
+    let(:language) { create(:language) }
+    let(:user) { create(:user) }
+    let(:skill) { create(:skill, user:, language:) }
+    let!(:comment1) { create(:comment, user:, skill:) }
+    let!(:comment2) { create(:comment, user:, skill:) }
+
+    describe '.resolve' do
+      it 'fetch a comment' do
+        post '/graphql',
+             params: { query: }
+
+        comment_data = JSON.parse(response.body)['data']['fetchComments']
+        expect(comment_data.length).to eq 2
+      end
+    end
+
+    def query
+      <<~GQL
+        query {
+          fetchComments {
+            id
+            body
+            skill {
+              	title
+              	language {
+                  name
+                }
+              	user {
+                  name
+                  email
+                }
+              }
+            user {
+              name
+              email
+            }
+          }
+        }
+      GQL
+    end
+  end
+end
