48a8be8d869a4ed498f45a795ddd8ae3008852bf
Matthias Haefeli <haefeli.matthias@gmail.com>
2022-02-16 18:41:04 -0600

    change language label (#40)
    Co-authored-by: matthiashaefeli <haefeli.matthias2gmail.com>

--- app/graphql/mutations/add_skill.rb
+++ app/graphql/mutations/add_skill.rb
@@ -8,14 +8,12 @@ module Mutations
     field :skill, Types::SkillType, null: false
 
     def resolve(params:, authenticate:)
-      skill_params = params.to_h
       token = authenticate.to_h[:token]
       user = AuthToken.verified_user(token)
       return unless user
 
       begin
-        skill = Skill.new(skill_params)
-        skill.user = user
+        skill = Skill.new(skill_params(params.to_h, user))
         skill.save!
 
         { skill: }
@@ -23,6 +21,13 @@ module Mutations
         GraphQL::ExecutionError.new("Invalid attributes for #{e.record.class}:"\
           " #{e.record.errors.full_messages.join(', ')}")
       end
+    end
+
+    def skill_params(params, user)
+      language = Language.find_by(label: params[:language])
+      params[:user] = user
+      params[:language] = language
+      params
     end
   end
 end
