fd1aa348581a7b9ed94803d81300014acd765771
Matthias Haefeli <haefeli.matthias@gmail.com>
2022-02-26 09:16:16 -0600

    add status and page to fetchuserskill (#43)
    Co-authored-by: matthiashaefeli <haefeli.matthias2gmail.com>

--- app/graphql/queries/fetch_skills_by_user.rb
+++ app/graphql/queries/fetch_skills_by_user.rb
@@ -4,9 +4,13 @@ module Queries
   class FetchSkillsByUser < Queries::BaseQuery
     type [Types::SkillType], null: false
     argument :user_id, ID, required: true
+    argument :status, ID, required: true
+    argument :page, ID, required: true
 
-    def resolve(user_id:)
-      Skill.where(user_id: user_id).order(created_at: :desc)
+    def resolve(user_id:, status:, page:)
+      p = page.to_i
+      offset = p == 1 ? 0 : p * 10
+      Skill.where(user_id: user_id).where(status: status).limit(20).offset(offset).order(created_at: :desc)
     end
   end
 end
