e7e9b5840cdbb4df7c6aa0bd7c3d8c69b53d1692
matthiashaefeli <haefeli.matthias2gmail.com>
2022-03-06 11:08:19 -0600

    remove logger

--- config/environment.rb
+++ config/environment.rb
@@ -3,14 +3,3 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-Rails.logger = Logger.new(STDOUT)
-Rails.logger.level = Logger::DEBUG
-Rails.logger.datetime_format = "%Y-%m-%d %H:%M:%S"
-
-class MyAppFormatter < Logger::Formatter
-  def call(severity, time, programName, message)
-      "#{datetime}, #{severity}: #{message} from #{programName}\n"
-  end
-end
-
