f342b180750f925bd2f93c6d7279198c7810af9f
matthiashaefeli <haefeli.matthias2gmail.com>
2022-03-16 16:07:48 -0500

    remember email created

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,8 +36,16 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
-  host = 'localhost:3000'
-  config.action_mailer.default_url_options = { :host => 'localhost:3000', protocol: 'http' }
+  # host = 'localhost:3000'
+  # config.action_mailer.default_url_options = { :host => 'localhost:3000', protocol: 'http' }
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => ENV['USER_NAME'],
+    :password => ENV['USER_VERIFICATION'],
+    :authentication => "plain",
+    :enable_starttls_auto => true,
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
---
+++ spec/mailers/previews/remember_mailer_preview.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# Preview all emails at http://localhost:3000/rails/mailers/remember_mailer
+class RememberMailerPreview < ActionMailer::Preview
+end
