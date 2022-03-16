02ada46d370efcb1f96a146aa51186b090252572
Matthias Haefeli <haefeli.matthias@gmail.com>
2022-03-16 16:09:22 -0500

    remember email created (#61)
    Co-authored-by: matthiashaefeli <haefeli.matthias2gmail.com>

---
+++ app/mailers/remember_mailer.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class RememberMailer < ApplicationMailer
+  def month_remember_email
+    @skill = params[:skill]
+    @user = @skill.user
+
+    mail(to: @user.email, subject: 'Easy Remember Me: 1 Month ago')
+  end
+
+  def five_month_remember_email
+    @skill = params[:skill]
+    @user = @skill.user
+
+    mail(to: @user.email, subject: 'Easy Remember Me: 5 Months ago')
+  end
+
+  def yearly_remember_email
+    @year_number = params[:years]
+    @skill = params[:skill]
+    @user = @skill.user
+
+    mail(to: @user.email, subject: "Easy Remember Me: #{@year_number} Years ago")
+  end
+end
--- spec/mailers/remember_mailer_spec.rb
+++ spec/mailers/remember_mailer_spec.rb
@@ -2,6 +2,6 @@
 
 require 'rails_helper'
 
-RSpec.describe VerifyMailer, type: :mailer do
+RSpec.describe RememberMailer, type: :mailer do
   pending "add some examples to (or delete) #{__FILE__}"
 end
