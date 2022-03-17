c9e2e10fbf6ebb2c43c940d3c282f4a614cdf7ed
Matthias Haefeli <haefeli.matthias@gmail.com>
2022-03-17 18:43:49 -0500

    add remember mailer and remember job (#62)
    Co-authored-by: matthiashaefeli <haefeli.matthias2gmail.com>

---
+++ app/jobs/month_remember_job.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class MonthRememberJob < ApplicationJob
+  include SkillHelper
+
+  def perform
+    skills = fetch_skills(1.month).where('user_settings.first_month = ?', true)
+    skills.each do |skill|
+      RememberMailer.month_remember_email(skill).deliver_later
+    end
+  end
+end
---
+++ app/jobs/year_remember_job.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class YearRememberJob < ApplicationJob
+  include SkillHelper
+
+  def perform
+    skills = fetch_skills(1.year).where('user_settings.year = ?', true)
+    skills.each do |skill|
+      RememberMailer.month_remember_email(skill).deliver_later
+    end
+  end
+end
