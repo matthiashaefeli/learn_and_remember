be2ca653ad99231db4d1d9063f2fe7a8b8fccb89
Matthias Haefeli <haefeli.matthias@gmail.com>
2022-04-21 19:17:46 -0500

    Email body (#74)
    * email body
    
    * reminder functions and email body
    
    Co-authored-by: matthiashaefeli <haefeli.matthias2gmail.com>

--- app/jobs/skill_helper.rb
+++ app/jobs/skill_helper.rb
@@ -9,4 +9,15 @@ module SkillHelper
          .or(Skill.where(status: 2))
          .where('skills.created_at BETWEEN ? AND ?', start_date, end_date)
   end
+
+  def fetch_skills_yearly()
+    start_date = DateTime.now.beginning_of_day - time
+    end_date = DateTime.now.end_of_day - time
+    Skill.eager_load(user: :user_setting)
+         .where(status: 1)
+         .or(Skill.where(status: 2))
+         .where('skills.created_at < ?', DateTime.now.beginning_of_day)
+         .where("EXTRACT(month FROM skills.created_at) = EXTRACT(month FROM now())")
+         .where("EXTRACT(day FROM skills.created_at) = EXTRACT(day FROM now())")
+  end
 end
